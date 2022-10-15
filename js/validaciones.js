ab2a6f1dd36e9dd15259fd52d89b2ded441d5d6f
RogelioBadachi <royer2302@gmail.com>
2022-10-15 03:42:32 -0700

    agregué validación a comparación de fechas

--- js/validaciones.js
+++ js/validaciones.js
@@ -6,11 +6,16 @@ inputNacimiento.addEventListener("blur", (evento) => {
 
 function validarNacimiento (input) {
     const fechaCliente = new Date(input.value);
-    mayorDeEdad(fechaCliente);
+    let mensaje = "";
+    if(!mayorDeEdad(fechaCliente)){
+        mensaje = "Debes tener al menos 18 años de edad";
+    }
+
+    input.setCustomValidity(mensaje);
 }
 
 function mayorDeEdad (fecha) {
     const fechaActual = new Date();
     const diferenciaFechas = new Date(fecha.getUTCFullYear() + 18, fecha.getUTCMonth(), fecha.getUTCDate());
-    console.log(diferenciaFechas <= fechaActual);
+    return diferenciaFechas <= fechaActual;
 }
